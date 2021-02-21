a98c76e0fde88c6ca043e6365fdf4dc62809791c
Romashka <oblivionz1111@gmail.com>
2021-02-21 19:01:32 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+'use strict'
+
+let logo = document.getElementById("menu");
+
+
+window.onload = function() {
+    TweenLite.from(logo, 1, {opacity:0, bottom:"300px"});    
+  }
+
+
+
+$(document).ready(function() {
+  $('.choosePizza').slick();
+});
+
+
+
+  //cmd-0 to restart
+
+
